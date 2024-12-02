c348408379f95833c8776c9c2cb19d2559c95293
turing <filipe.candeias@ua.pt>
2024-12-02 11:02:51 +0000

    5ª tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 //ficheiro de script
+const form = document.getElementById('formContato');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    if (form.checkValidity() === false) {
+        event.stopPropagation();
+    } else {
+            alert('Formulário enviado com sucesso!');
+    }
+
+    form.classList.add('was-validated');
+});
